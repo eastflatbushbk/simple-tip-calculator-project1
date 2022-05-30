9dab86ebde159176256d003014e19cd9dfe91bc5
Robert Sainton <robertsainton@outlook.com>
2022-05-30 15:30:24 -0400

    updating codes for incrementing using +button

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const increment = document.getElementById('plus');
 const decrement = document.getElementById('minus')
-let counterEl = document.getElementById('counter');
+let bill = document.getElementById('totalbill');
+
 increment.addEventListener('click', function () {
-    counterEl.textContent = number++;
+
+    let someNum = parseInt(bill.innerHTML);
+    bill.innerHTML = someNum + 1;
+    
+    decrement.disabled = false;
+    //bill.textContent = number++;
   });
   decrement.addEventListener('click', function () {
     counterEl.textContent = number--;
